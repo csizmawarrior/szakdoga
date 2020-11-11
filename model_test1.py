f5877573c1fbf1038edfd29dad1b52bc9476e86b
csizmawarrior <kulcsar.dana89@gmail.com>
2020-11-11 14:04:35 +0100

    the py file with a part of the model

---
+++ model_test1.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from torchvision import datasets
+from transformers import AutoTokenizer, AutoModel
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-multilingual-cased")
+model = AutoModel.from_pretrained("bert-base-multilingual-cased")
+
+#we add 2 more to it, because we will use it to measure the sentences for
+#the tokenizer, which has a starting and an end character additionally
+
+def longest_sentence_size(sentences):
+    current_max=0
+    for sentence in sentences:
+        if(len(sentence)>current_max):
+            current_max=len(sentence)
+    return current_max+2
+
+#it wants the sentences as a list already split to words/punctuation
+#in the first column
+def text_tokenizing(text_dataframe):
+    input_ids=[]
+    attention_masks=[]
+    targets=[]
+    for_preparing=tokenizer("")
+    start_symbol=for_preparing.input_ids[0]
+    end_symbol=for_preparing.input_ids[-1]
+    
+    for i in range(text_dataframe["sentence"].size):
+        target_idx_counter=text_dataframe["target_idx"][i]
+        input_ids_sentence=[]
+        attention_masks_sentence=[]
+        input_ids_sentence.append(start_symbol)
+        attention_masks_sentence.append(1)
+        
+        for j in range(len(text_dataframe["sentence"][i])):
+            
+            #a word is text_daraframe.iloc[i,0][j]
+            tokenized_word=tokenizer(
+                                     text_dataframe["sentence"][i][j],
+                                     add_special_tokens=False
+                                    )
+            
+            input_id=tokenized_word.input_ids
+            attention_mask=tokenized_word.attention_mask
+                
+            if(text_dataframe["sentence"][i][j]==text_dataframe["word"][i]):
+                target=len(input_id)
+                text_dataframe["target_idx"][i]=len(input_ids_sentence)           
+            
+            input_ids_sentence.extend(input_id)
+            attention_masks_sentence.extend(attention_mask)
+        
+        input_ids_sentence.append(end_symbol)
+        attention_masks_sentence.append(1)
+        input_ids.append(input_ids_sentence)
+        attention_masks.append(attention_masks_sentence)
+        targets.append(target)
+        
+    #padding    
+    max_length_ids=longest_sentence_size(input_ids)
+    for i in range(len(input_ids)):
+        current_size=len(input_ids[i])
+        additional_zeros=[]
+        for j in range(max_length_ids-current_size):
+            additional_zeros.append(0)
+        
+        input_ids[i]=input_ids[i][0:-1]+additional_zeros+[input_ids[i][-1]]
+        attention_masks[i]=attention_masks[i][0:-1]
+                +additional_zeros
+                +[attention_masks[i][-1]]
+
+    tokenized_text=pd.DataFrame({
+        'sentence': text_dataframe["sentence"],
+        'word': text_dataframe["word"],
+        'target_idx': text_dataframe["target_idx"],
+        'target_length': targets,
+        'type': text_dataframe["type"],
+        'input_id': input_ids,
+        'attention_mask': attention_masks
+    })
+    return tokenized_text
+
